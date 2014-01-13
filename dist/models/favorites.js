2acf821d9df9ffec5ef3d32af6cb8d84913b82a4
whwnow <whwnow@gmail.com>
2014-01-13 11:30:02 +0800

    添加收藏日期字段,返回列表时按时间排序

--- dist/models/favorites.js
+++ dist/models/favorites.js
@@ -1,14 +1,16 @@
 var mongo = require('./db');
 var EventProxy = require('eventproxy');
 var ObjectID = require('mongodb').ObjectID;
+var moment = require('moment');
 
 function Favorite(item) {
-  this.userName = item.name;
-  this.newsId = item.username;
+  this.userName = item.userName;
+  this.newsId = item.newsId;
+  this.createTime = moment().format("YYYY-MM-DD HH:mm:ss");
 }
 module.exports = Favorite;
 
-Favorite.get = function(username, callback) {
+Favorite.get = function(userName, callback) {
   mongo.openCheck(function(error, db) {
     var ep;
     if (error) {
@@ -17,10 +19,11 @@ Favorite.get = function(username, callback) {
     ep = new EventProxy();
     ep.on('ids', function(list) {
       var i = 0,
+        j = 0,
         idArr = [];
       for (i = 0; i < list.length; i++) {
         idArr.push(ObjectID(list[i].newsId));
-      };
+      }
       db.collection('news', function(error, collection) {
         if (error) {
           return callback(error);
@@ -39,31 +42,50 @@ Favorite.get = function(username, callback) {
           if (error) {
             return callback(error);
           }
+          for (i = 0; i < list.length; i++) {
+            for (j = 0; j < docs.length; j++) {
+              if (docs[j]._id === list[i].newsId) {
+                docs[j].createTime = list[i].createTime;
+                break;
+              }
+            }
+          }
+          docs.sort(function(a, b) {
+            if (a.createTime > b.createTime) {
+              return 1;
+            } else if (a < b) {
+              return -1;
+            } else {
+              return 0;
+            }
+          });
+
           callback(null, docs);
         });
       });
-    })
+    });
+
     db.collection('favorites', function(error, collection) {
       if (error) {
         return callback(error);
       }
       collection.find({
-        username: username
+        userName: userName
       }, {
         _id: 0,
-        username: 1,
-        newsId: 1
+        newsId: 1,
+        createTime: 1
       }).toArray(function(error, docs) {
         if (error) {
           return callback(error);
         }
-        ep.fire('ids', docs)
+        ep.fire('ids', docs);
       });
     });
   });
 };
 
-Favorite.del = function(username, newsId, callback) {
+Favorite.del = function(userName, newsId, callback) {
   mongo.openCheck(function(error, db) {
     if (error) {
       return callback(error);
@@ -75,7 +97,7 @@ Favorite.del = function(username, newsId, callback) {
       }
 
       favorite = {
-        username: username,
+        userName: userName,
         newsId: newsId
       };
 
@@ -91,8 +113,8 @@ Favorite.del = function(username, newsId, callback) {
   });
 };
 
-Favorite.add = function(username, newsId, callback) {
-  console.log(username, newsId);
+Favorite.add = function(userName, newsId, callback) {
+
   mongo.openCheck(function(error, db) {
     if (error) {
       return callback(error);
@@ -103,10 +125,10 @@ Favorite.add = function(username, newsId, callback) {
         return callback(error);
       }
 
-      favorite = {
-        username: username,
+      favorite = new Favorite({
+        userName: userName,
         newsId: newsId
-      };
+      });
 
       collection.insert(favorite, {
         safe: true
